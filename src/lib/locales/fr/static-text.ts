9dbca6ed5da6e8437cd3393d110eeeb885540295
Cyril PERIE <cyril.perie96@gmail.com>
2025-06-15 23:25:06 +0200

    :art: feat: add static text and prepare i18n.

---
+++ src/lib/locales/fr/static-text.ts
@@ -0,0 +1,8 @@
+const staticText = {
+    metadata: {
+        title: "Unknow",
+        description: "I don't know what i want to do but i will do it the cleanest",
+    },
+}
+
+export default staticText;
