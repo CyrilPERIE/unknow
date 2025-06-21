0bb8936ed1c75f7bc2e21b2a1b0e8350e77cad64
Cyril PERIE <cyril.perie96@gmail.com>
2025-06-21 16:32:35 +0200

    :lock: Give access to certain pages only to non authenticated users.

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useUser } from "@/app/hooks/user/use-user";
+import routes from "@/lib/routes/routes";
+import { redirect } from "next/navigation";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated } = useUser();
+  if (isAuthenticated) {
+    redirect(routes.dashboard);
+  }
+  return <div>{children}</div>;
+}
